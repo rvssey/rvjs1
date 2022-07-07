d2c2c32cf02f0a2e543b5d2887e6170d4ffeff6e
rvssey <rvssey@gmail.com>
2022-07-07 13:53:46 +0400

    added middlewares

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const express = require('express')
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 const port = 3000;
+
+//Middlewares
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(cors())
 
 mongoose.connect(process.env.DATABASE, {useNewUrlParser:true, useUnifiedTopology:true,}).then(()=>{
     console.log("db Connected Successfully");
